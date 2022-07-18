a39e2d9a6c7d39760d23c6c7259b5390e8b12578
fengxxc <fengxxc@gmail.com>
2022-07-18 19:56:05 +0800

    feat(util): load json file support

--- src/StoryUtil.ts
+++ src/StoryUtil.ts
@@ -4,24 +4,29 @@ import fs from 'fs'
 import { ErrorHandler } from 'inkjs/engine/Error'
 import { PosixFileHandler } from 'inkjs/compiler/FileHandler/PosixFileHandler'
 import { InkParser } from 'inkjs/compiler/Parser/InkParser'
+import inkjs from 'inkjs'
 
 export default class StoryUtil {
     static getChoicesInlineKeyboard(runtimeStory: RuntimeStory, storyName: string) {
         const choices = runtimeStory.currentChoices.map(choice => ({
             text: choice.text,
-            callback_data: "choice:" + storyName + ":" + choice.index
+            callback_data: "choice:" + storyName + ":" + choice.originalThreadIndex + "_" + choice.index
         }))
         // console.log(choices)
         return choices
     }
 
     static loadRuntimeStory(storyName: string): RuntimeStory|null {
-        const inkFireBaseDir = path.join(__dirname, "../ink")
         // storyName "tests.ink"
         if (!storyName.endsWith('.ink')) {
             storyName += '.ink'
         }
+        const inkFireBaseDir = path.join(__dirname, "../ink")
         console.log(path.join(inkFireBaseDir, storyName))
+        return StoryUtil.loadRuntimeStoryFromInkFile(inkFireBaseDir, storyName)
+    }
+
+    static loadRuntimeStoryFromInkFile(inkFireBaseDir: string, storyName: string): RuntimeStory|null {
         const filePath = path.join(inkFireBaseDir, storyName)
         if (!fs.existsSync(filePath)) {
             console.error('Story file is not exists')
@@ -38,6 +43,12 @@ export default class StoryUtil {
         if (runtimeStory == null) {
             console.error('Story is null')
         }
+        return runtimeStory
+    }
+
+    static loadRuntimeStoryFromJsonFile(jsonFilePath: string): RuntimeStory {
+        const json = fs.readFileSync(jsonFilePath, "utf-8").replace(/^\uFEFF/, '')
+        const runtimeStory = new inkjs.Story(json)
         return runtimeStory
     }
 }
